8c797f6acff75c48e016bfb7efa767c7117fbca2
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-12 18:25:06 +0300

    Thread Pool Holds Join Handles
    Thread pool now holds join handles of size new(size).(just like a closure is defined)

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,9 +1,29 @@
-pub struct ThreadPool; 
+use std::thread;
+
+pub struct ThreadPool{
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     //we’ll use this 4 threads as the number of elements in a collection of threads, which is what the usize type is for
+    //because function panics its a good idea to use some documentaion 
+    ///Creates a new Thread pool of usize == size.
+    ///The size is the number of threads in the pool
+    /// 
+    /// #Panics 
+    /// 
+    /// The new function panics is the size is zero 
     pub fn new(size: usize) -> ThreadPool{
-        ThreadPool 
+        //validate that usize > 0 
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size{
+            //create threads
+        }
+
+        ThreadPool {threads}
     }
 
     pub fn execute<F>(&self,f:F)
