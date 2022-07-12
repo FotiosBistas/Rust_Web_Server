0a37845bc6c85220d0b1fc9c71d33482532c2ff5
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-07-12 17:35:54 +0300

    404 and refactoring of the ifs
    Use if let pattern for make if more readable

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -25,22 +25,24 @@ fn handle_connection(mut stream:TcpStream){
     let get = b"GET / HTTP/1.1\r\n"; 
     
     //handle the get request 
-    if buffer.starts_with(get) {
-        let status_line = "HTTP/1.1 200 OK\r\n\r\n"; 
-        let contents = fs::read_to_string("hello.html").unwrap(); 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}", 
-            status_line, 
-            contents.len(), 
-            contents, 
-        );
-        //println!("Response: {}",response); 
-        //error handling here 
-        stream.write(response.as_bytes()).unwrap(); 
-        stream.flush().unwrap();
+    let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")  
+       
     }else {
-        //handle some other request 
-    }
+        ("HTTP/1.1 404 NOT FOUND","404.html") 
+    }; 
+
+    let contents = fs::read_to_string(filename).unwrap(); 
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
     //println!("Read: {}",String::from_utf8_lossy(&buffer[..])); 
      
